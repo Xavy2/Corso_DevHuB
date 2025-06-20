45527725a8c0d9115e473150373d235f78661c27
Xavy2 <xavy.noris@gmail.com>
2025-06-20 17:18:03 +0200

    Completati chiamate /addUser, /addFilm e /listMovies

--- esercizioDEVHUB.js
+++ esercizioDEVHUB.js
@@ -1,140 +1,179 @@
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const app = express();
 
-const app = express()
-
-//middlewares per la gestione delle richieste in entrata
-app.use(express.json())
+// Middleware per la gestione delle richieste in entrata
+app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const config = {
   PORT: 3000,
-  TOKEN_SIGN_KEY: 'SecretKey123!',
+  TOKEN_SIGN_KEY: 'SecretKey123!', 
   MONGODB_URI: `mongodb+srv://Admin:Admin@cluster0.vcqdtqb.mongodb.net/`,
   MONGODB_DB: 'sample_mflix'
-}
+};
 
-// Creazione oggetto di connessione a mongodb
+// Creazione oggetto di connessione a MongoDB
 const client = new MongoClient(config.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
-})
+});
 
-//middleware controllo validità token - eseguito per tutte le richieste ricevute indistintamente dal path
+// Middleware per il controllo della validità del token
 app.use(function (req, res, next) {
-  //escludo rotte che non devono essere sotto autenticazione
   if (req.originalUrl === '/login' || req.originalUrl === '/addUser') {
-    return next()
+    return next();
   }
   if (req.headers.authorization && req.headers.authorization.length > 0 && req.headers.authorization.split(' ')[0] === 'Bearer') {
-    // lettura token presente nel header "authorization" della richiesta http
-    const token = req.headers.authorization.split(' ')[1]
+    const token = req.headers.authorization.split(' ')[1];
+    //console.log('Received token:', token); // Log del token
     try {
-      //verifica e recupero contenuto token ricevuto
-      const decoded = jwt.verify(token, config.TOKEN_SIGN_KEY)
-      //salvataggio contenuto del token in un campo del oggetto json della richiesta http ricevuta in modo da averlo disponibile all'interno del codice che risponde alla richiesta
-      req.user = decoded
-      //procedo con il richiamo del codice della richiesta effettiva
-      next()
+      const decoded = jwt.verify(token, config.TOKEN_SIGN_KEY);
+      req.user = decoded; // Salvo il contenuto del token nella richiesta
+      next();
     } catch (err) {
-      console.error(err)
-      //risposta in caso di errore nella validazione del token
-      res.status(403).json({ rc: 1, msg: err.toString() })
+      //console.error('Token verification error:', err); // Log dell'errore
+      res.status(403).json({ rc: 1, msg: err.toString() });
     }
   } else {
-    //risposta in caso di assenza del token nella richiesta
-    res.status(400).json({ rc: 1, msg: 'Missing token in request' })
+    res.status(400).json({ rc: 1, msg: 'Missing token in request' });
   }
-})
+});
 
 // Effettua il login con le credenziali fornite nel body della richiesta 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   try {
-    // connessione a mongodb
-    await client.connect()
-    // imposto il db in cui devo effettuare la query
-    const db = client.db(config.MONGODB_DB)
-    // cerco se esite già un utente con lo username che ho ricevuto
-    const user = await db.collection('users').findOne({ username: username });
-    // in caso non esiste rispondo alla richiesta indicando che l'utente non esiste
+    // Connessione a MongoDB
+    await client.connect();
+    const db = client.db(config.MONGODB_DB);
+    
+    // Cerco se esiste già un utente con lo username fornito
+    const user = await db.collection('users').findOne({ name: username });
     if (!user) return res.status(404).json({ rc: 1, msg: `User ${username} not found` });
-
-    // controllo che la password ricevuta nella richiesta corrisponda a quella salvata sul database
+    
+    // Controllo che la password fornita corrisponda a quella salvata nel database
     const match = await bcrypt.compare(password, user.password);
-    // in caso non corrispondesse rispondo alla richiesta indicando che le credenziali ricevute non sono valide
-    if (!match) return res.status(401).json({ rc: 1, msg: 'Invalid credentials' })
-    //se sono arrivato qui vuol dire che i controlli precedenti sono stati superati
-    const content = { username }
-    // genero quindi un token e gli imposto una durata di validità (1 ora in questo caso)
-    const token = jwt.sign(content, config.TOKEN_SIGN_KEY, {expiresIn: '1h'})
-    // invio la risposta alla richiesta con il token
+    if (!match) return res.status(401).json({ rc: 1, msg: 'Invalid credentials' });
+    
+    // Genero un token e imposto una durata di validità (1 ora)
+    const content = { username };
+    const token = jwt.sign(content, config.TOKEN_SIGN_KEY, { expiresIn: '1h' });
+    
+    // Invio la risposta alla richiesta con il token
     res.status(200).json({ rc: 0, msg: 'Login successful', token: token });
   } catch (err) {
-    console.error(err)
-    res.status(500).json({ rc: 1, msg: err.toString() })
+    console.error(err);
+    res.status(500).json({ rc: 1, msg: err.toString() });
   } finally {
-    // sia con che senza errori chiudo la connessione a mongodb
-    await client.close()
+    await client.close();
   }
-})
+});
 
 // Creazione di un nuovo utente con le credenziali fornite nel body della richiesta
 app.put('/addUser', async (req, res) => {
-  try {
-    // leggo i parametri (obbligatori) username, password e email ricevuti nel body della richiesta
-    // apro la connessione a mongodb
-    await client.connect()
-    // controllo se esiste già un utente con lo stesso username e se esiste rispondo con un messaggio di errore adeguato
-    // effettuo lo stesso controllo anche per il campo email
-    // se supero i controlli precedenti allora posso inserire il nuovo utente nel database
-    // effettua la insert sulla collection users e invia la risposta alla richiesta 
-    res.status(201).send({ rc: 0, msg: `User ${username} added successfully` })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ rc: 1, msg: err.toString() })
-  } finally {
-    await client.close()
+  const { username: name, password, email } = req.body; 
+  if (!name || !password || !email) {
+    return res.status(400).json({ rc: 1, msg: 'Username, password, and email are required.' });
   }
-})
+  try {
+    // Connessione a MongoDB
+    await client.connect();
+    const db = client.db(config.MONGODB_DB);
+    
+    // Controllo se l'username esiste già nel database
+    const user = await db.collection('users').findOne({ name });
+    if (user) return res.status(409).json({ rc: 1, msg: `User ${name} already exists.` });
+
+    // Controllo se l'email è già in uso
+    const mail = await db.collection('users').findOne({ email });
+    if (mail) return res.status(409).json({ rc: 1, msg: `Email ${email} is already in use by another user.` });
+    
+    // Hash della password
+    const salt = await bcrypt.genSalt(10);
+    const pwCrypt = await bcrypt.hash(password, salt);
+    
+    // Inserimento del nuovo utente
+    const newUser = { name, password: pwCrypt, email };
+    const data = await db.collection('users').insertOne(newUser);
+    
+    res.status(201).json({ rc: 0, msg: `User ${name} added successfully with ID ${data.insertedId}` });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ rc: 1, msg: err.toString() });
+  } finally {
+    await client.close();
+  }
+});
 
 // Aggiunta di un nuovo film con i dati forniti nel body della richiesta
 app.post('/addFilm', async (req, res) => {
-  try {
-     // leggo i parametri (obbligatori) title, director e year ricevuti nel body della richiesta
-     // se i parametri non sono tutti correttamente valorizzati rispondo con un messaggio di errore adeguato
-     // apro la connessione a mongodb
-     await client.connect()
-     // inserisco il nuovo film nella collection movies
-    res.status(201).send({ rc: 0, msg: `Film ${title} added successfully` })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ rc: 1, msg: err.toString() })
-  } finally {
-    await client.close()
+  const { title, director, year } = req.body;
+  if (!title || !director || !year) {
+    return res.status(400).json({ rc: 1, msg: 'Title, director e year are required' });
   }
-})
 
-// ritorna una lista filtrata di film  
+  try {
+    await client.connect();
+    const db = client.db(config.MONGODB_DB);
+
+    // Controlla se il titolo esiste già
+    const existing = await db.collection('movies').findOne({ title });
+    if (existing) {
+      return res.status(409).json({ rc: 1, msg: `Movie with title ${title} already present` });
+    }
+
+    const newFilm = { title, director, year };
+    await db.collection('movies').insertOne(newFilm);
+
+    res.status(201).json({ rc: 0, msg: `Movie ${title} successfully added` });
+  } catch (err) {
+    res.status(500).json({ rc: 1, msg: 'Server error: ' + err.message });
+  } finally {
+    await client.close();
+  }
+});
+
 app.get('/listMovies', async (req, res) => {
-    // leggo i filtri ricevuti nel body della richiesta in formato json
-    // apro la connessione a mongodb
-    // imposto il database su cui voglio lavorare
-    // effettuo la query e recupero i primi 50 record che trovo, ordinati in maniera decrescente per campo _id
-    // rispondo alla richiesta ritornado un campo data nel body della risposta che contiene i record recuperati dalla query
+  try {
+    const filters = req.query || {};
+    await client.connect();
+    const db = client.db(config.MONGODB_DB);
+    let query = {};
+    if (filters.title)
+      query.title = { $regex: filters.title, $options: "i" };
+    if (filters.director)
+      query.director = { $regex: filters.director, $options: "i" };
 
-   res.status(200).json({ rc: 0 })
-})
+    if (filters.year)
+      query.year = parseInt(filters.year, 10);
 
-// attivazione web server in ascolto sulla porta indicata
+    const movies = await db.collection('movies')
+      .find(query)
+      .sort({ _id: -1 })
+      .limit(50)
+      .toArray();
+
+    res.status(200).json({ rc: 0, data: movies });
+
+  } catch (err) {
+    res.status(500).json({ rc: 1, msg: err.message });
+  } finally {
+    await client.close();
+  }
+});
+
+
+
+// Attivazione web server in ascolto sulla porta indicata
 app.listen(config.PORT, () => {
-  console.log(`Movie Manager app listening on port ${config.PORT}`)
-})
+  console.log(`Movie Manager app listening on port ${config.PORT}`);
+});
